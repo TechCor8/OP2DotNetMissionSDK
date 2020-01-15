0ff07b2f1b5202bf318267ba43cdc50a84b0247f
TechCor8 <48194787+TechCor8@users.noreply.github.com>
2020-01-15 01:11:50 -0500

    Removed NativeSDK (Pre-submodule)

--- NativeMissionSDK/NativeSDK/HFL/Source/TriggerEx.cpp
+++
@@ -1,3 +0,0 @@
-#include "HFL.h"
-
-// TODO: TriggerEx implementation
--- NativeMissionSDK/NativeSDK/Outpost2DLL/Outpost2DLL.cpp
+++
@@ -1,4 +0,0 @@
-// This is a dummy source file for what is effectively a header only library.
-// Adding it to the project allows the project to build, which runs all
-// included headers through the compiler, checking for syntax errors.
-#include "Outpost2DLL.h"
